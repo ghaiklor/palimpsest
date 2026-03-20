f0ec8ec03094a83ef51a10fc2f908fc3fd5b51db
Eugene Obrezkov <ghaiklor@gmail.com>
2026-03-20 14:33:49 +0200

    [sector]: implement a Widget to draw a sector
    Signed-off-by: Eugene Obrezkov <ghaiklor@gmail.com>

---
+++ lib/sector/sector_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:palimpsest/sector/sector.dart';
+import 'package:palimpsest/sector/sector_painter.dart';
+
+class SectorWidget extends StatelessWidget {
+  final Sector sector;
+
+  const SectorWidget({super.key, required this.sector});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final blockSize = constraints.maxWidth / sector.columns;
+
+        return CustomPaint(
+          size: Size(constraints.maxWidth, constraints.maxHeight),
+          painter: SectorPainter(sector: sector, blockSize: blockSize),
+        );
+      },
+    );
+  }
+}
