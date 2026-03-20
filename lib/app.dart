ccb3d93e17e1353c1d18c091dd53dbe25f09d2ca
Eugene Obrezkov <ghaiklor@gmail.com>
2026-03-20 21:31:40 +0200

    [app]: apply Palimpsest theme to the CupertinoApp
    Signed-off-by: Eugene Obrezkov <ghaiklor@gmail.com>

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:palimpsest/themes/palimpsest_theme.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp();
+    return CupertinoApp(theme: PalimpsestTheme.themeData);
   }
 }
