4b2d33d964927f75777792e710df76ba647c5e28
Eugene Obrezkov <ghaiklor@gmail.com>
2026-03-20 11:59:52 +0200

    [block]: implement an .empty() factory constructor
    Signed-off-by: Eugene Obrezkov <ghaiklor@gmail.com>

--- lib/block/block.dart
+++ lib/block/block.dart
@@ -6,4 +6,8 @@ class Block {
   final BlockStyle style;
 
   const Block({required this.type, required this.style});
+
+  factory Block.empty() {
+    return Block(type: BlockType.empty, style: BlockStyle.of(BlockType.empty));
+  }
 }
