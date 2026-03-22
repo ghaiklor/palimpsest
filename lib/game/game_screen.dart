f567d358ecd3a08f7f4eaab53eb3b705dc4b2164
Eugene Obrezkov <ghaiklor@gmail.com>
2026-03-22 23:07:46 +0200

    [game]: add a defrag button to the game screen
    Signed-off-by: Eugene Obrezkov <ghaiklor@gmail.com>

--- lib/game/game_screen.dart
+++ lib/game/game_screen.dart
@@ -8,6 +8,7 @@ class GameScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = CupertinoTheme.of(context);
     // TODO: sector must not be created in the game screen
     final sector = Sector.withWeightedBlocks(50, 30, {
       Block.empty(): 0.4,
@@ -16,9 +17,34 @@ class GameScreen extends StatelessWidget {
 
     return CupertinoPageScaffold(
       child: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Center(child: SectorWidget(sector: sector)),
+        child: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SectorWidget(sector: sector),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: CupertinoButton(
+                  onPressed: () {},
+                  color: const Color(0xFF00FF41),
+                  child: Text(
+                    'DEFRAG',
+                    textAlign: TextAlign.center,
+                    style: theme.textTheme.textStyle.copyWith(
+                      fontSize: 14.0,
+                      letterSpacing: 4.0,
+                      color: CupertinoColors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
