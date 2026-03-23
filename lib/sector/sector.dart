9934202821b37450337902f52034a449b8671547
Eugene Obrezkov <ghaiklor@gmail.com>
2026-03-23 10:20:12 +0200

    [sector]: implement defrag() that will defrag one block
    Signed-off-by: Eugene Obrezkov <ghaiklor@gmail.com>

--- lib/sector/sector.dart
+++ lib/sector/sector.dart
@@ -1,18 +1,34 @@
 import 'dart:math';
 
+import 'package:flutter/foundation.dart';
 import 'package:palimpsest/block/block.dart';
+import 'package:palimpsest/block/block_type.dart';
 
-class Sector {
+class Sector extends ChangeNotifier {
   final List<Block> cells;
 
   final int rows;
   final int columns;
 
-  const Sector({
-    required this.cells,
-    required this.rows,
-    required this.columns,
-  });
+  Sector({required this.cells, required this.rows, required this.columns});
+
+  bool defrag() {
+    final emptyCellIndex = cells.indexWhere((c) => c.type == BlockType.empty);
+    if (emptyCellIndex == -1) return false;
+
+    final dataCellIndex = cells.indexWhere(
+      (c) => c.type == BlockType.data,
+      emptyCellIndex + 1,
+    );
+
+    if (dataCellIndex == -1) return false;
+
+    final temp = cells[emptyCellIndex];
+    cells[emptyCellIndex] = cells[dataCellIndex];
+    cells[dataCellIndex] = temp;
+    notifyListeners();
+    return true;
+  }
 
   factory Sector.withEmptyBlocks(int rows, int columns) {
     return Sector(
