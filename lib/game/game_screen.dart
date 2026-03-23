8845219edf56dce08928eece7415e4239bd33f15
Eugene Obrezkov <ghaiklor@gmail.com>
2026-03-23 10:41:29 +0200

    [game]: implement ticker that will defrag sector
    Signed-off-by: Eugene Obrezkov <ghaiklor@gmail.com>

--- lib/game/game_screen.dart
+++ lib/game/game_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:palimpsest/block/block.dart';
 import 'package:palimpsest/game/defrag_button.dart';
 import 'package:palimpsest/resources/io_resource.dart';
@@ -13,13 +14,47 @@ class GameScreen extends StatefulWidget {
   State<GameScreen> createState() => _GameScreenState();
 }
 
-class _GameScreenState extends State<GameScreen> {
+class _GameScreenState extends State<GameScreen> with TickerProviderStateMixin {
   final Sector sector = Sector.withWeightedBlocks(50, 30, {
     Block.empty(): 0.4,
     Block.data(): 0.6,
   });
 
+  static const _ioCostPerDefrag = 25.0;
+  static const _ioDefragInterval = 0.3;
   final IOResource _ioResource = IOResource();
+
+  late Ticker _ticker;
+  Duration _tickerLastElapsed = Duration.zero;
+  double _tickerAccumulated = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _ticker = createTicker(_onTick)..start();
+  }
+
+  @override
+  void dispose() {
+    _ticker.dispose();
+    super.dispose();
+  }
+
+  void _onTick(Duration elapsed) {
+    final dt = (elapsed - _tickerLastElapsed).inMicroseconds / 1000000.0;
+    _tickerLastElapsed = elapsed;
+    _tickerAccumulated += dt;
+
+    if (_tickerAccumulated >= _ioDefragInterval) {
+      _tickerAccumulated = 0.0;
+
+      if (_ioResource.value >= _ioCostPerDefrag) {
+        final moved = sector.defrag();
+        if (moved) _ioResource.decrementBy(_ioCostPerDefrag);
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
